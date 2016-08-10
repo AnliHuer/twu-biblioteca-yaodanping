9154c68b22b7b04e2f6e7015398ea37de9250768
Anlihuer <anlihuer@outlook.com>
2016-08-10 23:47:32 +0800

    add library class test

---
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,53 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    private Library library;
+
+    @Before
+    public void initLibrary() {
+        Book bookOne = new Book(1, "Andy", "1999-01-01");
+        Book bookTwo = new Book(2, "Alice", "1999-01-01");
+
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.add(bookOne);
+        bookList.add(bookTwo);
+
+        library = new Library(bookList);
+    }
+
+    @Test
+    public void getLibrary() throws Exception {
+        assertEquals(2, library.getLibrary().size());
+    }
+
+    @Test
+    public void setLibrary() throws Exception {
+        Book book = new Book(1, "Andy", "1999-01-01");
+
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.add(book);
+
+        library.setLibrary(bookList);
+        assertEquals(1, library.getLibrary().size());
+    }
+
+    @Test
+    public void showBookList() throws Exception {
+        String bookListString = "";
+
+        ArrayList<Book> list = library.getLibrary();
+        for (Book book : list) {
+            bookListString += book.getId() + "\t" + book.getAuthor() + "\t" + book.getPublicationDate() + "\n";
+        }
+
+        String expectedString = "1\tAndy\t1999-01-01\n2\tAlice\t1999-01-01\n";
+        assertEquals(expectedString, bookListString);
+    }
+}
