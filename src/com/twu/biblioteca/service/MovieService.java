ac1a6c4cc90ed90518f4974c3d44210745eea580
Anlihuer <anlihuer@outlook.com>
2016-08-12 13:23:20 +0800

    add checkout movie and return movie test and implement

--- src/com/twu/biblioteca/service/MovieService.java
+++ src/com/twu/biblioteca/service/MovieService.java
@@ -17,11 +17,40 @@ public class MovieService {
 
         for (int i = 0; i < movieList.size(); i++) {
             Movie movie = movieList.get(i);
-            libraryListString += movie.getId() + "\t" + movie.getName()+ "\t" + movie.getYear()+ "\t" + movie.getDirector()+"\t"+movie.getRate()+ "\n";
+            libraryListString += movie.getId() + "\t" + movie.getName() + "\t" + movie.getYear() + "\t" + movie.getDirector() + "\t" + movie.getRate() + "\n";
         }
 
         System.out.println(libraryListString);
         return libraryListString;
     }
-    
+
+    public void checkoutMovie(String movieId) {
+        for (int i = 0; i < this.movieList.size(); i++) {
+            Movie movie = this.movieList.get(i);
+
+            movie.isCheckout();
+
+            if (!movie.isCheckout() && movieId.equals(String.valueOf(movie.getId()))) {
+                movie.setCheckout(true);
+                System.out.println("Thank you! Enjoy the movie.");
+                return;
+            }
+        }
+
+        System.out.println("The movie is not available.");
+    }
+
+    public void returnMovie(String movieId) {
+        for (int i = 0; i < this.movieList.size(); i++) {
+            Movie movie = (Movie) this.movieList.get(i);
+
+            if (movie.isCheckout() && movieId.equals(String.valueOf(movie.getId()))) {
+                movie.setCheckout(false);
+                System.out.println("Thank you for returning the movie.");
+                return;
+            }
+        }
+
+        System.out.println("That is not a valid movie to return.");
+    }
 }
--- test/com/twu/biblioteca/service/MovieServiceTest.java
+++ test/com/twu/biblioteca/service/MovieServiceTest.java
@@ -1,6 +1,5 @@
 package com.twu.biblioteca.service;
 
-import com.twu.biblioteca.Constant;
 import com.twu.biblioteca.TestBase;
 import com.twu.biblioteca.bean.Movie;
 import org.junit.Before;
@@ -13,13 +12,13 @@ import static org.junit.Assert.assertEquals;
 public class MovieServiceTest extends TestBase {
 
     private ArrayList<Movie> movieArrayList = new ArrayList<>();
-    private MovieService movieService = new MovieService(new Constant().getMovieList());
+    private MovieService movieService = new MovieService(movieArrayList);
 
     @Before
     public void init() {
-        Movie movieOne = new Movie(1, "Hello world", "2013-01-01", "Tom", "1");
-        Movie movieTwo = new Movie(2, "Hello java", "2013-01-01", "Tom", "10");
-        Movie movieThree = new Movie(2, "Hello Junit", "2013-01-01", "Tom", "10");
+        Movie movieOne = new Movie(1, "Hello world", "2013-01-01", "Tom", "1", false);
+        Movie movieTwo = new Movie(2, "Hello java", "2013-01-01", "Tom", "10", true);
+        Movie movieThree = new Movie(3, "Hello Junit", "2013-01-01", "Tom", "10", true);
 
         ArrayList<Movie> list = new ArrayList<>();
         movieArrayList.add(movieOne);
@@ -34,4 +33,27 @@ public class MovieServiceTest extends TestBase {
         assertEquals(movieListString + "\n", bytes.toString());
     }
 
+    @Test
+    public void successCheckoutMovieTest(){
+        movieService.checkoutMovie("1");
+        assertEquals("Thank you! Enjoy the movie.\n", bytes.toString());
+    }
+
+    @Test
+    public void failCheckoutMovieTest(){
+        movieService.checkoutMovie("2");
+        assertEquals("The movie is not available.\n", bytes.toString());
+    }
+
+    @Test
+    public void successReturnMovieTest(){
+        movieService.returnMovie("2");
+        assertEquals("Thank you for returning the movie.\n", bytes.toString());
+    }
+
+    @Test
+    public void failReturnMovieTest(){
+        movieService.returnMovie("1");
+        assertEquals("That is not a valid movie to return.\n", bytes.toString());
+    }
 }
