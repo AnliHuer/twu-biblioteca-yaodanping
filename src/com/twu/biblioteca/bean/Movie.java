7c33ae0a96e6e5d9af9b0295ebea3c4ac869edbe
Anlihuer <anlihuer@outlook.com>
2016-08-12 12:34:08 +0800

    add movie class

---
+++ src/com/twu/biblioteca/bean/Movie.java
@@ -0,0 +1,40 @@
+package com.twu.biblioteca.bean;
+
+public class Movie {
+    private String name;
+    private String year;
+    private String director;
+    private String rate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+}
