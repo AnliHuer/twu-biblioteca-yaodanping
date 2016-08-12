2546da7d322b20223fc595f819ba81724277007e
Anlihuer <anlihuer@outlook.com>
2016-08-12 15:55:00 +0800

    user servie add userDetail

---
+++ src/com/twu/biblioteca/service/UserService.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca.service;
+
+import com.twu.biblioteca.bean.User;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+
+public class UserService {
+    private ArrayList<User> userArrayList = new ArrayList<>();
+
+    public UserService(ArrayList<User> userArrayList) {
+        this.userArrayList = userArrayList;
+    }
+
+    public void userDetail(String username) {
+        for (int i = 0; i < this.userArrayList.size(); i++) {
+            User user = this.userArrayList.get(i);
+
+            if (username.equals(user.getUsername())) {
+                System.out.println(user.getUsername() + "\t" + user.getEmail() + "\t" + user.getPhoneNumber() + "\n");
+                return;
+            }
+        }
+
+        System.out.println("Please input valid username");
+    }
+}
---
+++ test/com/twu/biblioteca/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package com.twu.biblioteca.service;
+
+import com.twu.biblioteca.TestBase;
+import com.twu.biblioteca.bean.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserServiceTest extends TestBase{
+    private ArrayList<User> userArrayList = new ArrayList<>();
+    private UserService userService = new UserService(userArrayList);
+
+    @Before
+    public void init(){
+        User userOne = new User("Tony", "12345", "1@qq.com","13112341234");
+        User userTwo = new User("Andy", "00000", "2@qq.com","13112341234");
+        User userThree = new User("Alice", "11111", "3@qq.com","13112341234");
+
+        userArrayList.add(userOne);
+        userArrayList.add(userTwo);
+        userArrayList.add(userThree);
+    }
+
+    @Test
+    public void userDetailTest(){
+        userService.userDetail("Andy");
+
+        assertEquals("Andy\t2@qq.com\t13112341234\n\n", bytes.toString());
+    }
+}
