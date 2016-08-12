865c71a0844db66fb24865e9d9310086ce81ea61
Anlihuer <anlihuer@outlook.com>
2016-08-12 11:26:40 +0800

    create class mainmenu

---
+++ src/com/twu/biblioteca/MainMenu.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.service.StartService;
+
+public class MainMenu {
+    
+    public static void main(String[] args) {
+       new StartService().start();
+    }
+}
+
+
+
